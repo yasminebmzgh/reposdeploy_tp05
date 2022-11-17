5c258294183dbae0d426d4f195f3192add8146aa
Yasmine Boumazzough <yasminebmzgh@MacBook-Air-de-Yasmine.local>
2022-11-17 16:51:13 +0100

    test

--- deploy/api/index.php
+++ deploy/api/index.php
@@ -18,6 +18,55 @@ $app->get('/api/hello/{name}', function (Request $request, Response $response, $
     return $response;
 });
 
+function createJwT (Response $response) : Response {
+
+    $issuedAt = time();
+    $expirationTime = $issuedAt + 60;
+    $payload = array(
+    'userid' => 'toto',
+    'email' => 'titi@gmail.com',
+    'pseudo' => 'titiPseudo',
+    'iat' => $issuedAt,
+    'exp' => $expirationTime
+    );
+
+    $token_jwt = JWT::encode($payload,JWT_SECRET, "HS256");
+    $response = $response->withHeader("Authorization", "Bearer {$token_jwt}");
+    return $response;
+}
+
+$app->get('/api/user', function (Request $request, Response $response, $args) {   
+    $data = array('nom' => 'toto', 'prenom' => 'titi','adresse' => '6 rue des fleurs', 'tel' => '0606060607');
+    $response->getBody()->write(json_encode($data));
+
+    return $response;
+});
+
+// APi d'authentification générant un JWT
+$app->post('/api/login', function (Request $request, Response $response, $args) {   
+    $err=false;
+    $body = $request->getParsedBody();
+    $login = $body ['login'] ?? "";
+    $pass = $body ['pass'] ?? "";
+
+    if (!preg_match("/[a-zA-Z0-9]{1,20}/",$login))   {
+        $err = true;
+    }
+    if (!preg_match("/[a-zA-Z0-9]{1,20}/",$pass))  {
+        $err=true;
+    }
+
+    if (!$err) {
+            $response = createJwT ($response);
+            $data = array('nom' => 'toto', 'prenom' => 'titi');
+            $response->getBody()->write(json_encode($data));
+     } else {          
+            $response = $response->withStatus(401);
+     }
+    return $response;
+});
+
+
 $options = [
     "attribute" => "token",
     "header" => "Authorization",
@@ -26,7 +75,7 @@ $options = [
     "algorithm" => ["HS256"],
     "secret" => JWT_SECRET,
     "path" => ["/api"],
-    "ignore" => ["/api/hello"],
+    "ignore" => ["/api/hello","/api/login","/api/createUser"],
     "error" => function ($response, $arguments) {
         $data = array('ERREUR' => 'Connexion', 'ERREUR' => 'JWT Non valide');
         $response = $response->withStatus(401);
