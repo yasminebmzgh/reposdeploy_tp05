59bc76d5b3e455554b8f4ef0e9863e496cc9870d
Yasmine Boumazzough <yasminebmzgh@macbook-air-de-yasmine.home>
2023-01-12 17:08:44 +0100

    test

--- deploy/api/index.php
+++ deploy/api/index.php
@@ -113,8 +113,130 @@ $app->post('/login', function (Request $request, Response $response) {
 	return $response;
 });
 
+$app->get('/clients', function (Request $request, Response $response) {
+	global $entityManager;
+	$clients = $entityManager->getRepository(Client::class)->findAll();
+	$response->getBody()->write(json_encode($clients));
+	return $response;
+});
+
+$app->get('/client', function (Request $request, Response $response) {
+	$token_header = $request->getHeader("Authorization")[0];
+
+	if(!preg_match('/Bearer\s(\S+)/', $token_header, $matches)) {
+		$response = $response->withStatus(401);
+		$response->getBody()->write("Invalid token");
+		return $response;
+	}
+	$token = JWT::decode($matches[1], new Key(JWT_SECRET, "HS256"));
+	$now = new DateTimeImmutable();
+	if($token->exp < $now->getTimestamp()) {
+		$response = $response->withStatus(401);
+		$response->getBody()->write("Token expired");
+		return $response;
+	}
+	global $entityManager;
+	$client = $entityManager->getRepository(Client::class)->find($token->data);
+	if ($client) {
+		$response->getBody()->write(json_encode(json_encode($client)));
+	} else {
+		$response = $response->withStatus(401);
+		$response->getBody()->write("Client not found");
+	}
+	return $response;
+});
+
+$app->post('/signup', function (Request $request, Response $response) {
+	$body = $request->getParsedBody();
+	$firstname = $body['firstname'] ?? "";
+	$lastname = $body['lastname'] ?? "";
+	$email = $body['email'] ?? "";
+	$phone = $body['phone'] ?? "";
+	$address = $body['address'] ?? "";
+	$gender = $body['gender'] ?? "";
+	$login = $body['login'] ?? "";
+	$password = $body['password'] ?? "";
+
+	if (empty($firstname) || empty($lastname) || empty($email) || empty($phone) || empty($address) || empty($gender) || empty($login) || empty($password) || !preg_match("/^[0-9]+$/", $phone) || !preg_match("/^[a-zA-Z0-9\s]+$/", $address) || !preg_match("/^[a-zA-Z0-9]+$/", $gender) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+		$response = $response->withStatus(401);
+		$response->getBody()->write("Bad request");
+		return $response;
+	}
+
+	global $entityManager;
+	$client = new Client();
+	$client->setFirstname($firstname);
+	$client->setLastname($lastname);
+	$client->setEmail($email);
+	$client->setPhone($phone);
+	$client->setGender($gender);
+	$client->setAddress($address);
+	$client->setLogin($login);
+	$client->setPassword($password);
+	$entityManager->persist($client);
+	$entityManager->flush();
+	$response->getBody()->write(json_encode($client));
+	return $response;
+});
+
+$app->put('/client', function (Request $request, Response $response) {
+	$body = $request->getParsedBody();
+	$id = $body['id'] ?? "";
+
+	if (empty($id) || !preg_match("/^[0-9]+$/", $id)) {
+		$response = $response->withStatus(401);
+		$response->getBody()->write("Missing id");
+		return $response;
+	}
+
+	global $entityManager;
+	$client = $entityManager->getRepository(Client::class)->find($id);
+	if(!$client) {
+		$response = $response->withStatus(401);
+		$response->getBody()->write("Client not found");
+		return $response;
+	}
+	$client = $body['firstname'] ? $client->setFirstname($body['firstname']) : $client;
+	$client = $body['lastname'] ? $client->setLastname($body['lastname']) : $client;
+	$client = $body['email'] ? $client->setEmail($body['email']) : $client;
+	$client = $body['phone'] ? $client->setPhone($body['phone']) : $client;
+	$client = $body['gender'] ? $client->setGender($body['gender']) : $client;
+	$client = $body['address'] ? $client->setAddress($body['address']) : $client;
+	$client = $body['login'] ? $client->setLogin($body['login']) : $client;
+	$client = $body['password'] ? $client->setPassword($body['password']) : $client;
+	$entityManager->persist($client);
+	$entityManager->flush();
+	$response->getBody()->write(json_encode($client));
+	return $response;
+});
+
+$app->delete('/client', function (Request $request, Response $response) {
+	$body = $request->getParsedBody();
+	$id = $body['id'] ?? "";
+
+	if (empty($id) || !preg_match("/^[0-9]+$/", $id)) {
+		$response = $response->withStatus(401);
+		$response->getBody()->write("Bad request");
+		return $response;
+	}
+
+	global $entityManager;
+	$client = $entityManager->getRepository(Client::class)->find($id);
+	if(!$client) {
+		$response = $response->withStatus(401);
+		$response->getBody()->write("Client not found");
+		return $response;
+	}
+	$entityManager->remove($client);
+	$entityManager->flush();
+	$response->getBody()->write("Client deleted");
+	return $response;
+});
+
+
 // Run app
 $app->add(new Tuupola\Middleware\JwtAuthentication($options));
+
 $app->run();
 
 ?>
