6b091a998e8d0634b1f3f266def5bde4a5e94e2d
Yasmine Boumazzough <yasminebmzgh@macbook-air-de-yasmine.home>
2023-01-12 16:10:17 +0100

    test

--- deploy/bootstrap.php
+++ deploy/bootstrap.php
@@ -1,16 +1,22 @@
 <?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
 use Doctrine\ORM\Tools\Setup;
 use Doctrine\ORM\EntityManager;
+
 date_default_timezone_set('America/Lima');
-require_once "vendor/autoload.php";
+
 $isDevMode = true;
-$config = Setup::createYAMLMetadataConfiguration(array(__DIR__ . "/config/yaml"), $isDevMode);
+$config = Setup::createYAMLMetadataConfiguration(array(__DIR__."/config/yaml"), $isDevMode);
 $conn = array(
-'host' => 'host',
-'driver' => 'pdo_pgsql',
-'user' => 'cnam_user',
-'password' => 'pass',
-'dbname' => 'cnam',
-'port' => '5432'
+	'host' => 'dpg-cf00fhqrrk0eqcopot2g-a.oregon-postgres.render.com',
+	'driver' => 'pdo_pgsql',
+	'user' => 'yasmine_db_user',
+	'password' => 'AJVxIVgIBYxWnGWYyumJcawZSrE6WoUa',
+	'dbname' => 'yasmine_db',
+	'port' => '5432'
 );
+
 $entityManager = EntityManager::create($conn, $config);
+
